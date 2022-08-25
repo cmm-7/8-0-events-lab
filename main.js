95413e3e88fca2e6e1fbf378aeb062836f4486ba
cmm-7 <carlosmendoza@pursuit.org>
2022-08-24 21:26:35 -0400

    events-lab complete

--- main.js
+++ main.js
@@ -7,3 +7,37 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+
+const colors = document.querySelectorAll("div.color");
+for(let color of colors){
+  color.addEventListener("click", (event) => {
+    let background = event.target.style.background;
+    const currentColor = document.querySelector("#current-color");
+    currentColor.style.background = background;
+  }) 
+} 
+
+const cellBoxes = document.querySelectorAll("div.cell")
+  for(let cellBox of cellBoxes){  
+  cellBox.addEventListener("click", () => {
+    const currentColor = document.querySelector("#current-color").style.background;
+    cellBox.style.background = currentColor;
+})
+}
+
+const div = document.createElement("div");
+div.classList.add("reset");
+const button = document.createElement("button");
+button.innerText = "Reset";
+button.classList.add("reset");
+const section = document.querySelector("#palette");
+section.after(div);
+div.append(button);
+
+button.addEventListener("click", () => {
+  for(let cellBox of cellBoxes){
+    cellBox.style.background = "white"
+  }
+})
+
